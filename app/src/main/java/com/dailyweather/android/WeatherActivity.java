2a556cfdc05c1f5cf4f11b54623f65a8d6e046bf
Tony <379728330@qq.com>
2019-04-04 13:38:18 +0800

    增加后台自动更新的功能

--- app/src/main/java/com/dailyweather/android/WeatherActivity.java
+++ app/src/main/java/com/dailyweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.dailyweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
@@ -26,6 +27,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.dailyweather.android.gson.Forecast;
 import com.dailyweather.android.gson.Weather;
+import com.dailyweather.android.service.AutoUpdateService;
 import com.dailyweather.android.util.HttpUtil;
 import com.dailyweather.android.util.Utility;
 
@@ -259,6 +261,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
